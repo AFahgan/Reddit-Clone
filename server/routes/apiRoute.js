a7fce8238a6ce388cc8f5b9773dc377a0b89e6ae
afahgan <afahgan@students.iugaza.edu.ps>
2022-03-26 15:53:04 +0200

    Create Home Page #20

--- server/routes/apiRoute.js
+++ server/routes/apiRoute.js
@@ -1,10 +1,9 @@
 const apiRoute = require("express").Router();
-const { postSignUp, signin, logout } = require("../controllers");
+const { postSignUp, signin } = require("../controllers");
 
 
 
 apiRoute.post("/register", postSignUp);
 apiRoute.post("/login", signin);
-apiRoute.get("/api/v1/logout", logout);
 
 module.exports = apiRoute;
--- server/routes/publicRoute.js
+++ server/routes/publicRoute.js
@@ -1,6 +1,7 @@
 const publicRoute = require("express").Router();
 const path = require("path");
 const { isAuthProtected, isUserLogined } = require("../controllers/middleware");
+const { logout } = require("../controllers");
 
 publicRoute.get("/home", (req, res) => {
   res.sendFile(
@@ -11,6 +12,9 @@ publicRoute.get("/register", isUserLogined,(req, res) => {
     path.join(__dirname, "..", "..", "public", "html", "register.html")
   );
 });
+
+publicRoute.get("/logout", logout);
+
 publicRoute.get("/login", isUserLogined, (req, res) => {
   res.sendFile(
     path.join(__dirname, "..", "..", "public", "html", "login.html")
