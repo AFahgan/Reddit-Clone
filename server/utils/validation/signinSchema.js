c4d97e7154daedce9339a946f5446e72d81a6cee
afahgan <afahgan@students.iugaza.edu.ps>
2022-03-28 16:51:36 +0300

    Creae Post #26

--- server/utils/validation/signinSchema.js
+++ server/utils/validation/signinSchema.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const signinSchema = Joi.object({
   email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .email({ minDomainSegments: 2,  tlds: { allow: false } })
     .required(),
   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
 });
--- server/utils/validation/signupSchema.js
+++ server/utils/validation/signupSchema.js
@@ -3,8 +3,8 @@ const Joi = require("joi");
 const signupSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(30).required(),
   email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
+  .email({ minDomainSegments: 2,  tlds: { allow: false } })
+  .required(),
   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
   confirmPassword: Joi.ref("password"),
 });
