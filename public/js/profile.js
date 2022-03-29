4cb4007c2b980e53a894d2e9da06dcf5fbb298eb
afahgan <afahgan@students.iugaza.edu.ps>
2022-03-29 11:38:25 +0300

    Creae Profile #24

--- public/js/profile.js
+++ public/js/profile.js
@@ -37,13 +37,12 @@ fetch('/logout').then((response) => {
 fetch("/userpost")
   .then((res) => res.json())
   .then((posts) => {
-    
     const postCard = document.querySelector(".redditposts");
     posts.forEach((post) => {
       const postdiv = document.createElement("div");
       postdiv.classList.add("redditpost");
       postCard.appendChild(postdiv);
-      console.log(post[i]);
+      console.log(post);
 
       const voteSection = document.createElement("div");
       voteSection.classList.add("vote-section");
