b6a4ff8ef0b1a83dc5676f55ef9f1502e1e50d57
afahgan <afahgan@students.iugaza.edu.ps>
2022-03-29 13:14:11 +0300

    Create Profile Page #24

--- server/database/queries/getPostsDB.js
+++ server/database/queries/getPostsDB.js
@@ -2,7 +2,7 @@ const connection = require('../config/connection');
 
 const getpostsDB = () => {
   const sql = {
-    text: "SELECT posts.id,posts.title, posts.content, posts.img_url,posts.user_id,users.userName FROM posts JOIN users ON (posts.user_id = users.id) ;",
+    text: "SELECT posts.id,posts.title, posts.content,  posts.votes,posts.img_url,posts.user_id,users.userName FROM posts JOIN users ON (posts.user_id = users.id) ;",
     values: [],
   };
 
